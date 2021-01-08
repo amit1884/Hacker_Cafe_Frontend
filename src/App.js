074590c9350154af4f3ebdb231b8be60fc34fbd2
amit1884 <amitraj200raj@gmail.com>
2021-01-08 18:32:21 +0530

    carousel added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-
+import Carousel from './components/Carousel'
+import './assets/styles/style.css'
 class App extends Component {
     render() {
         return (
             <div>
-                <h1>App</h1>
+                <Carousel/>
             </div>
         )
     }
